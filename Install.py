5eaa18ea3eeba8723032647adde5b4e54b7be930
Gilead Cosman <gileadcosman@gmail.com>
2025-03-17 23:34:01 -0700

    Updated Install.py

--- Install.py
+++ Install.py
@@ -4,3 +4,4 @@ if not os.path.isdir('blender-curve-to-svg'):
 	os.system('git clone https://github.com/OpenSourceJesus/blender-curve-to-svg.git --depth=1')
 if not os.path.isdir('tinifyjs'):
 	os.system('git clone https://github.com/OpenSourceJesus/tinifyjs.git --depth=1')
+import tinifyjs.Install
